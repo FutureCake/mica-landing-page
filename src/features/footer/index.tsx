c9bfb0715f777f29fd075bdd06f2d2d3b4e3b57c
FutureCake <thomas.c.hoek@gmail.com>
2025-08-21 15:53:19 +0200

    small fixes

--- src/features/footer/index.tsx
+++ src/features/footer/index.tsx
@@ -1,11 +1,14 @@
+import useColorTheme from "../../shared/contexts/color/hook";
 import "./styles.scss";
 
 export default function Footer() {
 
+    const { text, primary } = useColorTheme();
+
     return (
-        <footer id="footer">
-            <p>Wow, you got all the way to the footer</p>
-            <p>I guess you could also follow us on <a href="https://www.instagram.com/app.mica.love">Instagram</a> now ;)</p>
+        <footer id="footer" style={{ color: text }}>
+            <p>Wow, you made it all the way to the bottom!</p>
+            <p>I guess you could also follow us on <a style={{ color: primary }} href="https://www.instagram.com/app.mica.love">Instagram</a> now ;)</p>
         </footer>
     )
 }
--- src/features/introduction/index.tsx
+++ src/features/introduction/index.tsx
@@ -8,7 +8,7 @@ export default function Introduction() {
     return (
         <div id="introduction">
             <p style={{ color: text }}>
-                <span style={{ color: primary, fontWeight: "bold", fontSize: 34 }}>Mica</span> is a modern loyalty program platform designed to help businesses engage with their customers effectively.<br />
+                <span style={{ color: primary }}>Mica</span> is a modern loyalty program platform designed to help businesses engage with their customers effectively.<br />
                 With intuitive tools and seamless integrations, Mica makes it easy to reward loyal customers, track engagement, and grow your brand. Whether you're a small business or a large enterprise, our platform adapts to your needs, helping you build lasting relationships and drive repeat business.
             </p>
         </div>
