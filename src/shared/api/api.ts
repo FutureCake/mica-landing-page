201bf0d4e4c5f1b237a16cdde4739cd30effe1df
FutureCake <thomas.c.hoek@gmail.com>
2025-08-21 15:43:06 +0200

    added api response type

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+export type APIResponse<T = undefined> = {
+    message: string;
+    data?: T;
+}
+
 export const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     timeout: import.meta.env.VITE_TIMEOUT,
