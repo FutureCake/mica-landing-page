5c300723f89aee9ec05e7934af6a6db3af625ad3
FutureCake <thomas.c.hoek@gmail.com>
2025-08-21 14:03:35 +0200

    added api instance

---
+++ src/shared/api/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    timeout: import.meta.env.VITE_TIMEOUT,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
